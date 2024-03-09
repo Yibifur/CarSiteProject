6919600cc67a5807e4eacd28c57cb8d9ed10bb13
Yibifur <103214248+Yibifur@users.noreply.github.com>
2024-03-09 14:53:57 +0300

    asfasf

--- Core/UdemyCarBook.Domain/obj/Debug/net8.0/UdemyCarBook.Domain.AssemblyInfo.cs
+++ Core/UdemyCarBook.Domain/obj/Debug/net8.0/UdemyCarBook.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UdemyCarBook.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c537538c48b6c4e5bc94268250911c1a2c094f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("UdemyCarBook.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UdemyCarBook.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Infrastructure/UdemyCarBook.Persistence/obj/Release/net8.0/UdemyCarBook.Persistence.AssemblyInfo.cs
+++ Infrastructure/UdemyCarBook.Persistence/obj/Release/net8.0/UdemyCarBook.Persistence.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("UdemyCarBook.Persistence")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c537538c48b6c4e5bc94268250911c1a2c094f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("UdemyCarBook.Persistence")]
